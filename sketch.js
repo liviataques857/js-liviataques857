1096efa3d6fe674c421b33166fa41e6bc1c81e4a
liviataques857 <j.livia@escola.pr.gov.br>
2024-10-09 11:17:15 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(600, 600);
+   background("#BAD897");
+}
+
+
+function draw() {
   
-  function draw() {
-    background(220);
+  stroke ("purple");
+  fill ("#875291");
+ 
+  
+  // console.log(mouseIsPressed);
+  
+  if (mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
   }
+}
